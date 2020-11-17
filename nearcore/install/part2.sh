26affd7f855a16892fa4ec8953812f655ed0155a
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2020-11-16 22:00:03 -0600

    Update part2.sh

--- nearcore/install/part2.sh
+++ nearcore/install/part2.sh
@@ -10,16 +10,17 @@ CONFIG_URL=https://s3.us-east-2.amazonaws.com/build.openshards.io/nearcore-deplo
 
 # Initialize Neard
 cd /var/lib/near-guildnet
-rm -rf /var/lib/near-guildnet/home/*
 ./neard --home /var/lib/near-guildnet/home init --account-id=$ACCOUNT_ID --chain-id=guildnet
 
-# Get correct files
+echo '* Getting the correct files and fixing permissions'
 wget $GENESIS_URL -O /var/lib/near-guildnet/home/genesis.json
 wget $CONFIG_URL -O /var/lib/near-guildnet/home/config.json
 sudo chmod 664 /var/lib/near-guildnet/home/*
 chmod 775 /var/lib/near-guildnet/home
 chmod 775 /var/lib/near-guildnet/
 chown -R guildnet_service:near -R /var/lib/near-guildnet
+
+
 echo "* Creating systemd unit file for NEAR validator service"
 cat > /lib/systemd/system/near-guildnet-validator.service <<EOF
 [Unit]
@@ -41,5 +42,8 @@ EOF
 ln -s /var/lib/near-guildnet/neard /usr/local/bin/neard
 ln -s /lib/systemd/system/near-guildnet-validator.service /etc/systemd/system/multi-user.target.wants/near-guildnet-validator.service
 chown -R guildnet_service:near /usr/local/bin/neard
+
+echo '* Cleaning up the mess we made'
+rm -rf /tmp/src/
 
 exit
