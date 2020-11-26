6d4a39d77fe3a598a4894a43be2712299cd665b2
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2020-11-25 22:18:33 -0600

    compile in lxc

--- nearcore/install/part1.sh
+++ nearcore/install/part1.sh
@@ -6,43 +6,33 @@ echo "* Starting the GUILDNET builder"
 RELEASE=$(lsb_release -c -s)
 NEAR_VERSION="1.16.2-guildnet"
 NEAR_REPO="https://github.com/crypto-guys/nearcore.git"
-ACCOUNT_ID=<ENTER YOUR VALIDATOR ID>
+ACCOUNT_ID=stakeu.staked.guildnet
 CHAINID=guildnet
 CONFIG_URL="https://s3.us-east-2.amazonaws.com/build.openshards.io/nearcore-deploy/guildnet/config.json"
-# Timezone list is available using command "timedatectl list-timezones" use appropriate timezone
-TIMEZONE=<FILL_THIS_IN>
+vm_name="script-test"
 
-echo "* Setting up the service account"
-groupadd near
-adduser --system --disabled-login --disabled-password --ingroup near --no-create-home guildnet
-usermod -aG near guildnet
-
-echo "* Fixing the time settings and locale"
-timedatectl set-timezone $TIMEZONE
-timedatectl set-ntp true
-# This generates the locale for english US UTF-8 if yours is different change it
-locale-gen en_US.UTF-8
-mkdir -p /var/lib/near/home/guildnet
+echo "* Launching LXC container to build in"
+lxc launch ubuntu:focal ${vm_name}
+lxc exec script-compiler /bin/bash --force-noninteractive
 
 echo "* Install Required Packages"
-# Prerequsits
-apt-get -qq update
-apt-get -qq upgrade
-apt-get -qq autoremove
-apt-get -qq autoclean
-apt-get -qq install git curl libclang-dev build-essential iperf llvm runc gcc g++ g++-multilib make cmake clang pkg-config libssl-dev libudev-dev libx32stdc++6-7-dbg lib32stdc++6-7-dbg python3-dev
-snap install rustup --classic
-rustup default nightly
-rustup component add clippy-preview
-rustup update
+lxc exec ${vm_name} -- /usr/bin/apt-get -qq update
+lxc exec ${vm_name} -- /usr/bin/apt-get -qq upgrade
+lxc exec ${vm_name} -- /usr/bin/apt-get -qq autoremove
+lxc exec ${vm_name} -- /usr/bin/apt-get -qq autoclean
+lxc exec ${vm_name} -- /usr/bin/apt-get -qq install git curl libclang-dev build-essential iperf llvm runc gcc g++ g++-multilib make cmake clang pkg-config libssl-dev libudev-dev libx32stdc++6-7-dbg lib32stdc++6-7-dbg python3-dev
+lxc exec ${vm_name} -- /usr/bin/snap install rustup --classic
+lxc exec ${vm_name} -- /usr/bin/rustup default nightly
+lxc exec ${vm_name} -- /usr/bin/rustup component add clippy-preview
+lxc exec ${vm_name} -- /usr/bin/rustup update
 
 # Compile Nearcore
-rm -rf /tmp/src
-mkdir -p /tmp/src/
-cd /tmp/src/ && git clone $NEAR_REPO
-cd nearcore
-git switch $NEAR_VERSION
-make release
+lxc exec ${vm_name} -- /usr/bin/rm -rf /tmp/src
+lxc exec ${vm_name} -- /usr/bin/mkdir -p /tmp/src/
+lxc exec ${vm_name} -- /usr/bin/cd /tmp/src/ && git clone $NEAR_REPO
+lxc exec ${vm_name} -- /usr/bin/cd nearcore
+lxc exec ${vm_name} -- /usr/bin/git switch $NEAR_VERSION
+lxc exec ${vm_name} -- /usr/bin/make release
 
-echo '* nearcore is now compiled in /tmp/src/guildnet/target/release'
-echo '* You should now run ./part2.sh'
+echo '* nearcore is now compiled ***'
+echo '* Not Finished Yet*
