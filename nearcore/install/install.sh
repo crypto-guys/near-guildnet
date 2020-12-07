00aa97b94cc8110bd23a2f85fb6a316424fd35fc
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2020-12-07 02:36:13 -0600

    tested: focal - working

--- nearcore/install/install.sh
+++ nearcore/install/install.sh
@@ -7,13 +7,21 @@ RELEASE=$(lsb_release -c -s)
 NEAR_VERSION=1.16.2-guildnet
 NEAR_REPO="https://github.com/crypto-guys/nearcore.git"
 vm_name="compiler"
+NAME=$(echo $USER)
 
+echo "* Updating via APT and installing required packages"
 sudo apt-get -qq update && sudo apt-get -qq upgrade
 sudo apt-get -qq install snapd squashfs-tools git curl python3
-sudo snap install lxd 
-sudo usermod -aG lxd $USER
+sleep 5
 
-# echo "* Initializing LXD"
+echo '* Install lxd using snap'
+sudo snap install lxd
+
+echo '* Pause 30 seconds'
+sleep 30
+sudo usermod -aG lxd $NAME
+
+echo "* Initializing LXD"
     cat <<EOF | sudo lxd init --preseed
 config:
   images.auto_update_interval: 15
@@ -43,12 +51,14 @@ profiles:
       type: disk
   name: default
 cluster: null
+
 EOF
 
-sudo snap restart lxd
-sleep 10
+sudo systemctl restart snapd
+sleep 15
 
-echo "* Launching LXC container to build in"
+echo "* Detected Ubuntu $RELEASE"
+echo "* Launching Ubuntu $RELEASE LXC container to build in"
 lxc launch ubuntu:$RELEASE ${vm_name}
 echo "* Pausing for 90 seconds while the container initializes"
 sleep 90
@@ -69,15 +79,15 @@ echo "* Switching Version"
 lxc exec ${vm_name} -- sh -c "cd /tmp/src/nearcore && git checkout 1.16.2-guildnet"
 echo "* Attempting to compile"
 lxc exec ${vm_name} -- sh -c "cd /tmp/src/nearcore && make release"
+lxc exec ${vm_name} -- sh -c "mkdir ~/binaries"
+lxc exec ${vm_name} -- sh -c "cd /tmp/src/nearcore/target/release/ && cp genesis-csv-to-json keypair-generator near-vm-runner-standalone neard state-viewer store-validator ~/binaries"
+lxc exec ${vm_name} -- sh -c "cd /tmp/src/nearcore/target/release/ && cp near ~/binaries/nearcore"
+lxc exec ${vm_name} -- sh -c "cd /tmp && tar -cf nearcore.tar -C ~/ binaries/"
 
-echo "* Creating tarball"
-lxc exec ${vm_name} -- sh -c "mkdir -p /src/binaries/ && cd /tmp/src/nearcore/target/release/ && cp -p genesis-csv-to-json keypair-generator near near-vm-runner-standalone neard state-viewer store-validator /src/binaries"
-lxc exec ${vm_name} -- sh -c "cd / && tar -cvf nearcore1.16.2-guildnet.tar -C /src/ binaries"
-
-echo "* Retriving the tarball and storing in /src/nearcore-guildnet"
-mkdir -p nearcore-guildnet
-lxc file pull ${vm_name}/tmp/binaries/nearcore1.16.2-guildnet.tar nearcore-guildnet/nearcore-guildnet.tar
-ls nearcore-guildnet/
+echo "* Retriving the tarball and storing in ~/nearcore.tar"
+sudo mkdir -p /usr/lib/near/guildnet
+sudo mkdir -p /tmp/near
+sudo lxc file pull ${vm_name}/tmp/nearcore.tar /tmp/near/nearcore.tar
 
 echo "* Guildnet Install Script Starting"
 
@@ -86,7 +96,7 @@ read VALIDATOR_ID
 
 # Script settings
 CONFIG_URL="https://s3.us-east-2.amazonaws.com/build.openshards.io/nearcore-deploy/guildnet/config.json"
-TARBALL="nearcore-guildnet/nearcore-guildnet.tar"
+TARBALL="/tmp/near/nearcore.tar"
 
 echo "* Setting up required accounts, groups, and privilages"
 sudo groupadd near
@@ -96,21 +106,19 @@ sudo adduser --system --disabled-login --disabled-password --ingroup near --no-c
 # export NODE_ENV=guildnet
 
 # Copy Guildnet Files to a suitable location
-sudo mkdir -p /var/lib/near/guildnet
-tar -xf $TARBALL
-cd binaries
-sudo mv /usr/local/bin/near /usr/local/bin/nearcore
-sudo cp * /usr/local/bin
-
+sudo mkdir -p /usr/lib/near/guildnet
+cd /tmp/near
+tar -xf nearcore.tar
+sudo cp -p /tmp/near/binaries/* /usr/local/bin
 
 echo '* Getting the correct files and fixing permissions'
-sudo neard --home /var/lib/near/guildnet/ init --download-genesis --chain-id guildnet --account-id $VALIDATOR_ID
-sudo wget $CONFIG_URL -O /var/lib/near/guildnet/config.json
-sudo chown -R neard-guildnet:near -R /var/lib/near
+sudo neard --home /usr/lib/near/guildnet/ init --download-genesis --chain-id guildnet --account-id $VALIDATOR_ID
+sudo wget $CONFIG_URL -O /usr/lib/near/guildnet/config.json
+sudo chown -R neard-guildnet:near -R /usr/lib/near
 
 echo "* Creating systemd unit file for NEAR validator service"
 
-sudo cat > /var/lib/systemd/neard-guildnet.service <<EOF
+sudo cat > /usr/lib/systemd/neard.service <<EOF
 [Unit]
 Description=NEAR GUILDNET Validator Service
 Documentation=https://github.com/nearprotocol/nearcore
@@ -121,7 +129,7 @@ After=network-online.target
 Type=exec
 User=neard-guildnet
 Group=near
-ExecStart=neard --home /var/lib/near/guildnet/ run
+ExecStart=neard --home /usr/lib/near/guildnet/ run
 Restart=on-failure
 RestartSec=80
 #StandardOutput=append:/var/log/guildnet.log
@@ -130,14 +138,46 @@ RestartSec=80
 WantedBy=multi-user.target
 EOF
 
-#udo ln -s /var/lib/systemd/neard-guildnet.service /etc/systemd/system
-#udo systemctl enable neard-guildnet.service
-sudo systemctl status neard-guildnet.service
+ln -s /usr/lib/systemd/neard.service /etc/systemd/system/neard.service
+
+echo '* Adding logfile conf for neard'
+sudo mkdir -p /usr/lib/systemd/journald.conf.d
+sudo cat > /usr/lib/systemd/journald.conf.d/neard.conf <<EOF
+#  This file is part of systemd
+#
+# This file controls the logging behavior of the service
+# You can change settings by editing this file.
+# Defaults can be restored by simply deleting this file.
+#
+# See journald.conf(5) for details.
+
+[Journal]
+
+Storage=auto
+ForwardToSyslog=no
+Compress=yes
+#Seal=yes
+#SplitMode=uid
+SyncIntervalSec=1m
+RateLimitInterval=30s
+#RateLimitBurst=1000
+
+EOF
+
+echo '* Service Status 'sudo systemctl status neard.service' *'
+sudo systemctl enable neard.service
+sudo systemctl status neard.service
 
 echo '* The installation has completed removing the installer'
 lxc stop compiler
 lxc delete compiler
-sudo snap remove --purge lxd
-rm -rf /tmp/guildnet
+rm -rf /tmp/near
 
-ehco '* You should now verify your validator key is correct"
+echo '* You should now verify your validator key is correct'
+cat /usr/lib/near/guildnet/validator_key.json | grep "account_id\|public_key"
+
+echo '* You can verify this is correct using near-cli on another machine the staking key for the account_id should match'
+
+echo "* near view <account_id>.guildnet get_staking_key '{}' "
+
+echo "* Please restart the machine now then use these commands as needed | systemctl start/stop/enable/disable neard | journalctl -u neard | journalctl --help"
