a8827b68339393685745a7baf82b5bead9fe1636
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2020-12-13 08:50:17 -0600

    Update install.sh

--- nearcore/install/install.sh
+++ nearcore/install/install.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
+# SCRIPT CONFIG
+# This script was created by # Rickrods @ crypto-solutions.net for the NEAR Guildnet Network
+# 
+
 set -eu
-# Script settings 
+
+# Get Ubuntu Version so we build the right one 
 RELEASE=$(lsb_release -c -s)
 # Change this to compile a different Version 
-NEAR_VERSION=1.16.2
+NEAR_VERSION="1.16.2"
 # Change this to use a different repo
 NEAR_REPO="https://github.com/solutions-crypto/nearcore.git"
 vm_name="compiler"
+ 
+############## 
+# User Section 
+# NOTE: You are not required to run the compile process it is options. 
+# but the file /tmp/near/nearcore.tar is required for the install portion of the script
+# The tar file contains 1 folder named binaries with all binaries inside of it 
+#######################################################################################################
 
-echo "* Starting the GUILDNET build process"
-echo "***  What is your validator accountId?  ***"
-read VALIDATOR_ID
+# Ensure the user has root privilages to execute the script
+if [ "$USER" != "root" ]
+then
+echo " You must run this script with sudo privilage hint: "sudo ./install.sh" -->>EXITING>>>!!!"
+exit
+fi
 
+echo "***  Do you want to compile the nearcore binaries?  y/n?  ***"
+read -r NEAR_COMPILE
+
+echo "***  Do you want to install the NEARD guildnet Service?  y/n?  ***"
+read -r NEARD_INSTALL
+
+if [ "$NEARD_INSTALL" == y ]
+then
+echo "***  Please input your validator-id  ***"
+read -r VALIDATOR_ID
+fi
+ 
+echo "*** PLEASE NOTE: if you choose to enable the autoremove feature the tar file with the nearcore binaries will be moved to /usr/local/share upon script completion"
+echo "***  Do you want to remove the install extra components and tmp files (LXD and /tmp/near) when finished? y/n?  ***"
+read -r AUTO_REMOVE
+
+#######################################################################################################
+# This section has all funcitons the script will use they are ignored unless called upon 
+#######################################################################################################
+
+# Update and install required packages 
 function update_via_apt
 {
     echo "* Updating via APT and installing required packages"
@@ -23,8 +59,8 @@ function update_via_apt
 
 }
 
-# sudo usermod -aG lxd $NAME
-
+# Initializes the container software with preseed information. 
+# NOTE: advanced init configs using "cloud-init" require cloud-tools and it is highly sugggested to use a cloud image
 function init_lxd
 {
 echo "* Initializing LXD"
@@ -39,11 +75,10 @@ networks:
   type: ""
   project: default
 storage_pools:
-- config:
-    size: 20GB
+- config: {}
   description: ""
   name: guildnet
-  driver: btrfs
+  driver: dir
 profiles:
 - config: {}
   description: ""
@@ -64,22 +99,31 @@ systemctl restart snapd
 sleep 15
 }
 
-function launch_container 
+function launch_container
 {
     echo "* Detected Ubuntu $RELEASE"
     echo "* Launching Ubuntu $RELEASE LXC container to build in"
-    lxc launch ubuntu:${RELEASE} ${vm_name}
-    echo "* Pausing for 90 seconds while the container initializes"
-    sleep 90
+
+    if [ "$RELEASE" == "focal" ]
+    then
+    lxc launch images:68d4b58311e0 ${vm_name}
+    fi
+    if [ "$RELEASE" == "bionic" ]
+    then
+    lxc launch images:88cd379f1e63 ${vm_name}
+    fi
+
+    echo "* Pausing for 15 seconds while the container initializes"
+    sleep 15
     echo "* Install Required Packages"
-    lxc exec ${vm_name} -- /usr/bin/apt-get -qq update
-    lxc exec ${vm_name} -- /usr/bin/apt-get -qq upgrade
-    lxc exec ${vm_name} -- /usr/bin/apt-get -qq autoremove
-    lxc exec ${vm_name} -- /usr/bin/apt-get -qq autoclean
-    lxc exec ${vm_name} -- /usr/bin/apt-get -qq install git curl libclang-dev build-essential iperf llvm runc gcc g++ g++-multilib make cmake clang pkg-config libssl-dev libudev-dev libx32stdc++6-7-dbg lib32stdc++6-7-dbg python3-dev
-    lxc exec ${vm_name} -- /usr/bin/snap install rustup --classic
-    lxc exec ${vm_name} -- /snap/bin/rustup default nightly
-    lxc exec ${vm_name} -- /snap/bin/rustup update
+    lxc exec ${vm_name} -- sh -c "apt-get -qq update"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq upgrade"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq autoremove"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq autoclean"
+    lxc exec ${vm_name} -- sh -c "apt-get -y install git curl snapd squashfs-tools libclang-dev build-essential iperf llvm runc gcc g++ g++-multilib make cmake clang pkg-config libssl-dev libudev-dev libx32stdc++6-7-dbg lib32stdc++6-7-dbg python3-dev"
+    lxc exec ${vm_name} -- sh -c "snap install rustup --classic"
+    lxc exec ${vm_name} -- sh -c "rustup default nightly"
+    lxc exec ${vm_name} -- sh -c "rustup update"
 }
 
 function compile_source
@@ -96,6 +140,7 @@ function compile_source
     lxc exec ${vm_name} -- sh -c "cd /tmp && tar -cf nearcore.tar -C ~/ binaries/"
 }
 
+# Create a tar file of the binaries and puts them in /tmp/near/nearcore.tar
 function get_tarball
 {
     echo "* Retriving the tarball and storing in /tmp/near/nearcore.tar"
@@ -105,91 +150,89 @@ function get_tarball
 }
 
 
-if [ $USER != "root" ]
+# This function is the main function that installes the components required for compiling the source 
+# It also compiles the code and exports the results in a tar file
+function compile_nearcore 
+{
+    update_via_apt
+    init_lxd
+    launch_container
+    compile_source
+    get_tarball
+    echo "***  The compile process has completed the binaries were stored in /tmp/near/nearcore.tar"
+}
+
+
+# Create Accounts and Group for new user 
+function create_user_and_group
+{
+    echo "* Guildnet Install Script Starting"
+    echo "* Setting up required accounts, groups, and privilages"
+    sudo groupadd near
+    sudo adduser --system --disabled-login --disabled-password --ingroup near --no-create-home neard-guildnet
+}
+
+# Creating a system service that will run with the non privilaged service account neard-guildnet
+function create_neard_service
+{
+    # Copy Guildnet Files to a suitable location
+    sudo mkdir -p /usr/lib/near/guildnet
+    sudo wget https://raw.githubusercontent.com/crypto-guys/near-guildnet/main/nearcore/install/neard.service --output-file /usr/lib/near/neard.service
+    cd /tmp/near
+    tar -xf nearcore.tar
+    sudo cp -p /tmp/near/binaries/* /usr/local/bin
+
+    echo '* Getting the correct files and fixing permissions'
+    sudo neard --home /usr/lib/near/guildnet/ init --download-genesis --chain-id guildnet --account-id "$VALIDATOR_ID"
+    sudo chown -R neard-guildnet:near -R /usr/lib/near
+
+    echo '* Adding logfile conf for neard'
+    sudo mkdir -p /usr/lib/systemd/journald.conf.d
+    sudo wget https://raw.githubusercontent.com/crypto-guys/near-guildnet/main/nearcore/install/near.conf --output-file /usr/lib/systemd/journald.conf.d/neard.conf
+
+    echo '* Service Status 'sudo systemctl status neard.service' *'
+    sudo systemctl enable neard.service
+    sudo systemctl disable neard.service
+    sudo systemctl status neard.service
+}
+
+function clean_up 
+{
+    echo '* Removing the LXC container used to compile the code. '
+    echo '* Removing LXD... We do not remove snapd because it now ships with UBUNTU by default and we find it to be quite useful  '
+    echo '* To manually remove snapd ---   sudo apt purge snapd --autoremove'
+    echo '* '
+    echo '* Creating a backup copy of tarbarll in /usr/local/share'
+    lxc stop compiler
+    lxc delete compiler
+    sudo snap remove --purge lxd
+    # sudo apt purge snapd --autoremove
+    cp /tmp/near/nearcore.tar /usr/local/share
+    rm -rf /tmp/near
+}
+
+# END Functions
+#######################################################################################################
+
+
+#######################################################################################################
+# Use user supplied input to determine which parts of the script to run
+if [ "$NEAR_COMPILE" == y ] || [ "$NEAR_COMPILE" == "yes" ]
 then
-echo " Run sudo su before starting the script please"
-exit
+compile_nearcore
 fi
-update_via_apt
-init_lxd
-launch_container
-compile_source
-get_tarball
 
+if [ "$NEARD_INSTALL" == "y" ] || [ "$NEARD_INSTALL" == "yes" ]
+then
+echo "* YOU MUST HAVE A TARFILE WITH THE BINARIES IN /tmp/near/nearcore.tar to install without running the compile first"
+sleep 10
+create_user_and_group
+create_neard_service
+fi
 
-echo "* Guildnet Install Script Starting"
-
-# Script settings
-TARBALL="/tmp/near/nearcore.tar"
-
-echo "* Setting up required accounts, groups, and privilages"
-sudo groupadd near
-sudo adduser --system --disabled-login --disabled-password --ingroup near --no-create-home neard-guildnet
-
-# Set env variable is not really required unless you use the near-cli on same machine
-# export NODE_ENV=guildnet
-
-# Copy Guildnet Files to a suitable location
-sudo mkdir -p /usr/lib/near/guildnet
-cd /tmp/near
-tar -xf nearcore.tar
-sudo cp -p /tmp/near/binaries/* /usr/local/bin
-
-echo '* Getting the correct files and fixing permissions'
-sudo neard --home /usr/lib/near/guildnet/ init --download-genesis --chain-id guildnet --account-id $VALIDATOR_ID
-sudo chown -R neard-guildnet:near -R /usr/lib/near
-
-echo "* Creating systemd unit file for NEAR validator service"
-
-sudo cat > /usr/lib/systemd/neard.service <<EOF
-[Unit]
-Description=NEAR GUILDNET Validator Service
-Documentation=https://github.com/nearprotocol/nearcore
-Wants=network-online.target
-After=network-online.target
-[Service]
-Type=exec
-User=neard-guildnet
-Group=near
-ExecStart=neard --home /usr/lib/near/guildnet/ run
-Restart=on-failure
-RestartSec=80
-#StandardOutput=append:/var/log/guildnet.log
-[Install]
-WantedBy=multi-user.target
-EOF
-
-ln -s /usr/lib/systemd/neard.service /etc/systemd/system/neard.service
-
-echo '* Adding logfile conf for neard'
-sudo mkdir -p /usr/lib/systemd/journald.conf.d
-sudo cat > /usr/lib/systemd/journald.conf.d/neard.conf <<EOF
-#  This file is part of systemd
-#
-# This file controls the logging behavior of the service
-# You can change settings by editing this file.
-# Defaults can be restored by simply deleting this file.
-#
-# See journald.conf(5) for details.
-[Journal]
-Storage=auto
-ForwardToSyslog=no
-Compress=yes
-#Seal=yes
-#SplitMode=uid
-SyncIntervalSec=1m
-RateLimitInterval=30s
-#RateLimitBurst=1000
-EOF
-
-echo '* Service Status 'sudo systemctl status neard.service' *'
-sudo systemctl enable neard.service
-sudo systemctl status neard.service
-
-echo '* The installation has completed removing the installer'
-lxc stop compiler
-lxc delete compiler
-#sudo snap remove --purge lxd
-rm -rf /tmp/near
+if [ "$AUTO_REMOVE" == "y" ] || [ "$AUTO_REMOVE" == "yes" ]
+then
+clean_up
+fi
 
 echo '* You should restart the machine now due to changes made to the logging system then check your validator key'
